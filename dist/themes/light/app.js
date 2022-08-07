63df2092b4741e5c76402a65f674882c0a8040c0
Diego <diegolnr3@gmail.com>
2022-08-06 23:04:36 -0500

    -

--- dist/themes/light/app.js
+++ dist/themes/light/app.js
@@ -86,9 +86,15 @@ function init() {
 
     html += `
 <div class="mdui-container">
-  <div class="mdui-container-fluid">
-    <div id="nav" class="mdui-toolbar nexmoe-item nav-style"> </div>
-    </div>
+<div class="mdui-container-fluid">
+<nav id="nav" class="flex" aria-label="Breadcrumb">
+    <ol class="inline-flex items-center space-x-1 md:space-x-3">
+        <li class="inline-flex items-center">
+
+        </li>
+    </ol>
+</nav>
+</div>
   <div class="mdui-container-fluid">
     <div id="head_md" class="mdui-typo nexmoe-item" style="display:none;padding: 20px 0;"></div>
     <div id="content" class="nexmoe-item"></div>
@@ -198,7 +204,7 @@ function nav(path) {
                 if (n == '') {
                     break;
                 }
-                html += `<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>r<a class="folder" href="/${cur}:${p}">${n}</a>`;
+                html += `<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg><a class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white" href="/${cur}:${p}">${n}</a>`;
             }
         }
     }
