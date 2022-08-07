b7f7abe225ed8930273de2201c7a21450f32080e
Diego <diegolnr3@gmail.com>
2022-08-06 22:34:14 -0500

    -

--- dist/themes/light/app.js
+++ dist/themes/light/app.js
@@ -198,7 +198,7 @@ function nav(path) {
                 if (n == '') {
                     break;
                 }
-                html += `<i class="mdui-icon material-icons mdui-icon-dark folder" style="margin:0;">chevron_right</i><a class="folder" href="/${cur}:${p}">${n}</a>`;
+                html += `<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>r<a class="folder" href="/${cur}:${p}">${n}</a>`;
             }
         }
     }
