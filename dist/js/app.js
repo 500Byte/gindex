34381298329a786be0aa9ae993025834de2b7881
Diego <diegolnr3@gmail.com>
2022-08-06 23:27:53 -0500

    -

--- dist/js/app.js
+++ dist/js/app.js
@@ -86,14 +86,9 @@ function init() {
 
     html += `
 <div class="mdui-container">
-<div class="mdui-container-fluid">
-    <nav id="nav" class="flex" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-1 md:space-x-3">
-            <li class="inline-flex items-center">
-        </li>
-        </ol>
-    </nav>
-</div>
+  <div class="mdui-container-fluid">
+    <div id="nav" class="mdui-toolbar nexmoe-item nav-style"> </div>
+    </div>
   <div class="mdui-container-fluid">
     <div id="head_md" class="mdui-typo nexmoe-item" style="display:none;padding: 20px 0;"></div>
     <div id="content" class="nexmoe-item"></div>
@@ -190,7 +185,7 @@ function nav(path) {
     // });
     // html += `</select>`;
 
-    html += `<a href="/${cur}:/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"><svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg> Inicio</a>`;
+    html += `<a href="/${cur}:/" class="mdui-typo-headline folder"><svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg> Inicio</a>`;
     if (!model.is_search_page) {
         var arr = path.trim('/').split('/');
         var  p  =  '/' ;
@@ -203,7 +198,7 @@ function nav(path) {
                 if (n == '') {
                     break;
                 }
-                html += `<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg><a class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white" href="/${cur}:${p}">${n}</a>`;
+                html += `<i class="mdui-icon material-icons mdui-icon-dark folder" style="margin:0;">chevron_right</i><a class="folder" href="/${cur}:${p}">${n}</a>`;
             }
         }
     }
